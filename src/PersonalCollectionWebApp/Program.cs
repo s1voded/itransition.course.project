34bc60ed5775a6ae51dcbd1105e95674429a5554
Aleksei Sivodedov <sivoded@gmail.com>
2024-03-01 20:30:53 +0400

    feat: require confirmed account false

--- src/PersonalCollectionWebApp/Program.cs
+++ src/PersonalCollectionWebApp/Program.cs
@@ -32,7 +32,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
