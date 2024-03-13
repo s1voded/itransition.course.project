a97a99a8ba6cc698eb6aebfec20565988999ee1a
Aleksei Sivodedov <sivoded@gmail.com>
2024-03-13 18:48:22 +0400

    feat: add new fileds for search items: username and collection theme

--- src/PersonalCollection.Persistence/Repositories/ItemRepository.cs
+++ src/PersonalCollection.Persistence/Repositories/ItemRepository.cs
@@ -33,21 +33,25 @@ namespace PersonalCollection.Persistence.Repositories
 
         public async Task<IEnumerable<Item>> SearchItems(string search)
         {
+            var searchCondition = $"\"{search}\"";
+
             return await GetAll()
-                .Include(i => i.Collection)
-                .ThenInclude(c => c.User)
+                .Include(i => i.Collection).ThenInclude(c => c.User)
+                .Include(i => i.Collection).ThenInclude(c => c.Theme)
                 .Include(i => i.Tags)
                 .Include(i => i.Comments)
                 .Where(i => 
-                EF.Functions.Contains(i.Name, search) || 
-                EF.Functions.Contains(i.CustomStrings, search) ||
-                EF.Functions.Contains(i.CustomTexts, search) ||
-                EF.Functions.Contains(i.CustomInts, search) ||
-                EF.Functions.Contains(i.CustomDates, search) ||
-                EF.Functions.Contains(i.Collection.Name, search) ||
-                EF.Functions.Contains(i.Collection.Description, search) ||
-                i.Tags.Any(t => EF.Functions.Contains(t.Name, search)) ||
-                i.Comments.Any(c => EF.Functions.Contains(c.Content, search)))
+                EF.Functions.Contains(i.Name, searchCondition) || 
+                EF.Functions.Contains(i.CustomStrings, searchCondition) ||
+                EF.Functions.Contains(i.CustomTexts, searchCondition) ||
+                EF.Functions.Contains(i.CustomInts, searchCondition) ||
+                EF.Functions.Contains(i.CustomDates, searchCondition) ||
+                EF.Functions.Contains(i.Collection.Name, searchCondition) ||
+                EF.Functions.Contains(i.Collection.Description, searchCondition) ||
+                EF.Functions.Contains(i.Collection.Theme.Name, searchCondition) ||
+                EF.Functions.Contains(i.Collection.User.UserName, searchCondition) ||
+                i.Tags.Any(t => EF.Functions.Contains(t.Name, searchCondition)) ||
+                i.Comments.Any(c => EF.Functions.Contains(c.Content, searchCondition)))
                 .ToListAsync();
         }
     }
