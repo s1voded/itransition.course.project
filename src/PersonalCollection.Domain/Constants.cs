66bec7ba313c3d129c343738b1c424c7eb63250e
Aleksei Sivodedov <sivoded@gmail.com>
2024-03-14 18:37:48 +0400

    refactor: use constants global; use one method with auth check for create/edit collections/items

--- src/PersonalCollection.Domain/Constants.cs
+++ src/PersonalCollection.Domain/Constants.cs
@@ -20,6 +20,7 @@
         public const string NavCreate = "create";
         public const string NavCollectionId = "{CollectionId:int}";
         public const string NavItemId = "{ItemId:int}";
+        public const string NavSearch = "search";
 
         public const string AppRouteUserManager = "/usermanager";
         public const string AppRouteCollections = $"/{NavCollections}";
@@ -29,11 +30,23 @@
         public const string AppRouteItemCreate = $"/{NavCollections}/{NavCollectionId}/{NavCreate}";
         public const string AppRouteItemDetails = $"/{NavItems}/{NavItemId}";
         public const string AppRouteItemEdit = $"/{NavItems}/{NavItemId}/{NavEdit}";
+        public const string AppRouteSearch = $"/{NavSearch}";
+
+        public const string ParameterSearchTag = "SearchTag";
+        public const string ParameterSearchString = "SearchString";
 
         public const string HubName = "/commentshub";
         public const string HubSendCommentsUpdate = "SendCommentsUpdate";
         public const string HubReceiveCommentsUpdate = "ReceiveCommentsUpdate";
         public const string HubJoinGroup = "JoinGroup";
         public const string HubLeaveGroup = "LeaveGroup";
+
+        public enum ActionType
+        {
+            CanManageInit,
+            Add,
+            Update,
+            Delete
+        }
     }
 }
