550ea33cd77f75d84a2f152e3bdc430bf5d9f3e6
Aleksei Sivodedov <sivoded@gmail.com>
2024-03-18 20:46:30 +0400

    feat: update/delete collection image; not show image component if image null; refactor

--- src/PersonalCollection.Application/Services/ImageStorageService.cs
+++ src/PersonalCollection.Application/Services/ImageStorageService.cs
@@ -1,11 +1,5 @@
 ﻿using Dropbox.Api.Files;
 using Dropbox.Api;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using PersonalCollection.Application.Models.Config;
 
@@ -20,28 +14,28 @@ namespace PersonalCollection.Application.Services
             _dropboxConfig = dropboxConfig;
         }
 
-        public async Task Upload(string filename, Stream stream)
+        public async Task Upload(string fileName, Stream stream)
         {
-            var path = $"/{_dropboxConfig.Value.Folder}/{filename}";
+            var path = $"/{_dropboxConfig.Value.Folder}/{fileName}";
 
-            using (var dbx = new DropboxClient(_dropboxConfig.Value.Token))
-            {
-                var updated = await dbx.Files.UploadAsync(path, WriteMode.Overwrite.Instance, body: stream);
-            }
+            using var dbx = new DropboxClient(_dropboxConfig.Value.Token);
+            var updated = await dbx.Files.UploadAsync(path, WriteMode.Overwrite.Instance, body: stream);
         }
 
-        public async Task<byte[]> Download(string filename)
+        public async Task<byte[]> Download(string fileName)
         {
-            var path = $"/{_dropboxConfig.Value.Folder}/{filename}";
+            var path = $"/{_dropboxConfig.Value.Folder}/{fileName}";
 
-            using (var dbx = new DropboxClient(_dropboxConfig.Value.Token))
-            {
-                using (var response = await dbx.Files.DownloadAsync(path))
-                {
-                    return await response.GetContentAsByteArrayAsync();
-                }
-            }
+            using var dbx = new DropboxClient(_dropboxConfig.Value.Token);
+            using var response = await dbx.Files.DownloadAsync(path);
+            return await response.GetContentAsByteArrayAsync();
+        }
 
+        public async Task Delete(string fileName)
+        {
+            var path = $"/{_dropboxConfig.Value.Folder}/{fileName}";
+            using var dbx = new DropboxClient(_dropboxConfig.Value.Token);
+            var result = await dbx.Files.DeleteV2Async(path);
         }
     }
 }
