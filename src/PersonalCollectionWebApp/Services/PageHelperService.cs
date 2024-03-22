d5e6e903af2ebf5223f5838abe0a828bc00d5f7c
Aleksei Sivodedov <sivoded@gmail.com>
2024-03-22 13:59:53 +0400

    feat: move likes to separate component

--- src/PersonalCollectionWebApp/Services/PageHelperService.cs
+++ src/PersonalCollectionWebApp/Services/PageHelperService.cs
@@ -1,4 +1,5 @@
 ﻿using PersonalCollection.Application.Models;
+using PersonalCollection.Domain.Entities;
 using static PersonalCollection.Domain.Constants;
 
 namespace PersonalCollectionWebApp.Services
@@ -6,6 +7,8 @@ namespace PersonalCollectionWebApp.Services
     public class PageHelperService
     {
         public string GetLocalTime(DateTime dateTime, int timeZoneOffset) => dateTime.Subtract(TimeSpan.FromMinutes(timeZoneOffset)).ToString();
+
+        public string? GetReactionUserName(ApplicationUser? user, string deletedUserName) => user != null ? user.UserName : deletedUserName;
 
         public string GetTagSearchHref(string tagName)
         {
